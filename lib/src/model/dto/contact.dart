4e5b7a4795126ec469abaa69ebc2438d47039593
moreira <contato@eduardoem.com.br>
2020-05-06 17:24:53 -0300

    Add contact DTO

---
+++ lib/src/model/dto/contact.dart
@@ -0,0 +1,15 @@
+class ContactDTO {
+  int id;
+  String name;
+  String email;
+  String phone;
+  String img;
+
+  ContactDTO({
+    this.id,
+    this.name,
+    this.email,
+    this.phone,
+    this.img
+  });
+}
