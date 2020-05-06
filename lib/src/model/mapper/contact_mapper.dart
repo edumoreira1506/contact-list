1418b5db76d101bd3fc794685ddc4bbd0703d2f9
moreira <contato@eduardoem.com.br>
2020-05-06 17:28:55 -0300

    Add contact mapper

---
+++ lib/src/model/mapper/contact_mapper.dart
@@ -0,0 +1,26 @@
+import 'package:contact_list/src/model/dto/contact.dart';
+
+class ContactMapper {
+  static ContactDTO toDTO(Map map) {
+    return ContactDTO(
+      email: map['email'],
+      id: map['id'],
+      phone: map['phone'],
+      img: map['img'],
+      name: map['name'],
+    );
+  }
+
+  static Map toMap(ContactDTO dto) {
+    Map map = {
+      'email': dto.email,
+      'phone': dto.phone,
+      'name': dto.name,
+      'img': dto.img,
+    };
+
+    if(dto.id != null) map['id'] = dto.id;
+
+    return map;
+  }
+}
